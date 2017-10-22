7c14de93ed2cc9cf5ede3eeccb98512c07a37619
Jost von Hardenberg <hardenberg@gmail.com>
2017-10-22 23:07:19 +0200

    some dot vectorization

--- src/rf/fitslopex.jl
+++ src/rf/fitslopex.jl
@@ -5,7 +5,7 @@ Return spectral slope (minus 1) of spatial spectrum `fx`
 """
 	function fitslopex(fx)
         xr=collect(1:length(fx));
-        (a,sx)=linreg(log(xr),log(fx))
+        (a,sx)=linreg(log.(xr),log.(fx))
 	sx=abs(sx)
 	sx=sx-1
 	return sx
