f00b828ecc6295c856a2eb228df5cb82c4f399a4
Jost von Hardenberg <hardenberg@gmail.com>
2018-06-07 12:17:50 +0200

    fix priority of conservation options

--- src/rf/downscale_spaceonly.jl
+++ src/rf/downscale_spaceonly.jl
@@ -15,7 +15,7 @@ rg=gaussianize(rs);
 # Prepare small scale random fields
 g=metagauss(f);
 
-# aggiustiamo l'ampiezza di f
+# fix the amplitude of f
 (pstg,pstgt)=fft3d(g); 
 pstg=pstg*((ns*ns)^2);
 (pstrg,pstrgt)=fft3d(rg); 
@@ -42,22 +42,21 @@ fm=fm.*weight;
 # We want the aggregated field to be the same as pa
 ii=find(isnan.(r)); fm[ii]=NaN;
 
-if(fsmooth)
-  fma=smoothconv(interpola(agg(fm,nas,1),ns,1),nas); 
- # fma=smoothconv(fm,nas);
-  ri=interpola(r,ns,1);
-  raa=smoothconv(ri,nas);
-  fm=raa./fma.*fm; #pa=p aggregato a L0 e T0;
-else
-#fm=mergespec_spaceonly(fm,raa,div(nas,2));
-  raa=agg(r,nas,1);
-  fma=agg(fm,nas,1);
-  ca=raa./fma; #pa=p aggregato a L0 e T0;
-  cai=interpola(ca,ns,1);
-  fm=cai.*fm;
-end
 if(fglob)
   fm=fm*mean(r)/mean(fm)
+elseif(fsmooth)
+    fma=smoothconv(interpola(agg(fm,nas,1),ns,1),nas); 
+ # fma=smoothconv(fm,nas);
+    ri=interpola(r,ns,1);
+    raa=smoothconv(ri,nas);
+    fm=raa./fma.*fm; #pa=p aggregato a L0 e T0;
+else
+#fm=mergespec_spaceonly(fm,raa,div(nas,2));
+    raa=agg(r,nas,1);
+    fma=agg(fm,nas,1);
+    ca=raa./fma; #pa=p aggregato a L0 e T0;
+    cai=interpola(ca,ns,1);
+    fm=cai.*fm;
 end
 
 return fm
