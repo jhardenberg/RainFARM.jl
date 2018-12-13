d749099cb872a4ed245d036edb9f209ed8a4f157
Jost von Hardenberg <j.vonhardenberg@isac.cnr.it>
2018-12-13 23:36:19 +0100

    missing linreg

--- src/rf/fitslopex.jl
+++ src/rf/fitslopex.jl
@@ -4,11 +4,13 @@
 Return spectral slope (minus 1) of spatial spectrum `fx`.
 """
 	function fitslopex(fx; kmin=1)
-        kmin=Int(kmin);
-        xr=collect(1:length(fx));
-        (a,sx)=linreg(log.(xr[kmin:end]),log.(fx[kmin:end]))
-	sx=abs(sx)
-	sx=sx-1
+        kmin = Int(kmin);
+        xr = collect(1:length(fx));
+        x = log.(xr[kmin:end])
+        y = log.(fx[kmin:end])
+        sx, a = [ x[:] ones(length(x))] \ y
+	sx = abs(sx)
+	sx = sx-1
 	return sx
 	end
 
